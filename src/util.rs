74d9eb85b92b9a61c8d7392da14db46450dd3025
Kvasszn <kvasszn@gmail.com>
2026-05-20 00:08:48 -0600

    Add inherited virtual functions to vtables

--- src/util.rs
+++ src/util.rs
@@ -1,3 +1,5 @@
+use serde::{Deserialize, Deserializer};
+
 pub fn sanitize_member_prefix(name: &str) -> String {
     name.chars()
         .map(|ch| {
@@ -8,4 +10,23 @@ pub fn sanitize_member_prefix(name: &str) -> String {
             }
         })
         .collect()
+}
+
+
+pub fn parse_address_u64<'de, D>(deserializer: D) -> Result<u64, D::Error>
+where
+    D: Deserializer<'de>,
+{
+    let s = String::deserialize(deserializer)?;
+    let clean_str = s.trim_start_matches("0x");
+    u64::from_str_radix(clean_str, 16).map_err(serde::de::Error::custom)
+}
+
+pub fn parse_address_u32<'de, D>(deserializer: D) -> Result<u32, D::Error>
+where
+    D: Deserializer<'de>,
+{
+    let s = String::deserialize(deserializer)?;
+    let clean_str = s.trim_start_matches("0x");
+    u32::from_str_radix(clean_str, 16).map_err(serde::de::Error::custom)
 }
